9c8ffe18632399bda513ce754759c59005d47b4a
chandrajyoti123 <chandrajyotiadil15062003@gmail.com>
2023-10-21 11:28:49 +0530

    delete,patch,put

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 const app=express();
 app.use(express.json())
 import dotenv from 'dotenv'
@@ -8,7 +9,7 @@ import mongoose, {model, Schema } from "mongoose";
 const connectMongodb= async()=>{
     const response=await mongoose.connect(process.env.ECOMMERCE_URI)
     if(response){
-        console.log("mongodb added successfully")
+        console.log("mongodb  added successfully")
     }
 }
 connectMongodb()
@@ -63,6 +64,110 @@ app.get('/productbyname', async(req,res)=>{
      })
 
 
+})
+
+app.delete('/product/:id', async(req,res)=>{
+    const {id}= req.params
+     await Product.deleteOne({_id:id})
+     
+
+    res.json({
+        " result":true,
+        " message":`successfully detelted product of Id ${id}`
+ 
+     })
+
+})
+
+app.put('/products/:_id', async(req,res)=>{
+      const {_id}=req.params
+      const {name, description,price,productImage,brand}=req.body
+      await Product.updateOne({_id:_id},{$set:{
+        name:name,
+        description:description,
+        price:price,
+        productImage:productImage,
+        brand:brand
+
+      }})
+
+      const updatedProductone= await Product.findOne({_id:_id})
+      if(!name){
+        res.json({
+            result:false,
+            message:"name is required"
+        })
+
+      }
+      if(!description){
+        res.json({
+            result:false,
+            message:"name is description"
+        })
+
+      }
+      if(!price){
+        res.json({
+            result:false,
+            message:"name is price"
+        })
+
+      }
+      if(!productImage){
+        res.json({
+            result:false,
+            message:"name is productImage"
+        })
+
+      }
+      if(!brand){
+        res.json({
+            result:false,
+            message:"name is brand"
+        })
+
+      }
+
+    res.json({
+        " result":true,
+         "prductc":updatedProductone,
+        " message":`product updated of  Id ${_id}`
+ 
+     })
+
+})
+
+app.patch('/product/:_id', async(req,res)=>{
+const {_id}=req.params;
+const {name, description,price,productImage,brand}=req.body
+const Productone= await Product.findOne({_id:_id})
+if(name){
+    Productone.name=name
+}
+if(description){
+    Productone.description=description
+}
+if(price){
+    Productone.price=price
+}
+if(productImage){
+    Productone.productImage=productImage
+}
+if(brand){
+    Productone.brand=brand
+}
+
+const saved=  await Productone.save()
+
+ 
+    
+    res.json({
+        " result":true,
+         "prductc":saved,
+        " message":`product updated `
+ 
+     })
+    
 })
 
 const port=8080
